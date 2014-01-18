9f89a42056b5daba7635539c428cd87e4a944ab3
Enrique Pérez Arnaud <enriquepablo@gmail.com>
2014-01-18 14:02:38 +0100

    Update models.py
    Use settings.AUTH_USER_MODEL rather than django.contrib.auth.models.User

--- notification/models.py
+++ notification/models.py
@@ -7,8 +7,6 @@ from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 from django.utils.translation import get_language, activate
 
-from django.contrib.auth.models import User
-
 from notification import backends
 
 
@@ -79,7 +77,7 @@ class NoticeSetting(models.Model):
     of a given type to a given medium.
     """
 
-    user = models.ForeignKey(User, verbose_name=_("user"))
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("user"))
     notice_type = models.ForeignKey(NoticeType, verbose_name=_("notice type"))
     medium = models.CharField(_("medium"), max_length=1, choices=NOTICE_MEDIA)
     send = models.BooleanField(_("send"))
