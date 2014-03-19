bddcac23ff51dee285b4a4420bad3f437fa0eb05
Pablo Caro Revuelta <correo@pablocaro.es>
2014-03-19 12:27:31 +0100

    Backend for sending html mails

--- notification/backends/email.py
+++ notification/backends/email.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 from django.utils.translation import ugettext
 
@@ -40,3 +41,46 @@ class EmailBackend(backends.BaseBackend):
         }, context)
 
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
+
+
+class EmailHtmlBackend(backends.BaseBackend):
+    spam_sensitivity = 2
+
+    def can_send(self, user, notice_type):
+        can_send = super(EmailHtmlBackend, self).can_send(user, notice_type)
+        if can_send and user.email:
+            return True
+        return False
+
+    def deliver(self, recipient, sender, notice_type, extra_context):
+        # TODO: require this to be passed in extra_context
+
+        context = self.default_context()
+        context.update({
+            "recipient": recipient,
+            "sender": sender,
+            "notice": ugettext(notice_type.display),
+        })
+        context.update(extra_context)
+
+        messages = self.get_formatted_messages((
+            "short.txt",
+            "full.txt"
+        ), notice_type.label, context)
+
+        subject = "".join(render_to_string("notification/email_subject.txt", {
+            "message": messages["short.txt"],
+        }, context).splitlines())
+
+        body = render_to_string("notification/email_body.txt", {
+            "message": messages["full.txt"],
+        }, context)
+
+        html_body = render_to_string("notification/email_body.txt", {
+            "message": messages["full.txt"],
+        }, context)
+
+        # send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
+        msg = EmailMultiAlternatives(subject, body, settings.DEFAULT_FROM_EMAIL, [recipient.email])
+        msg.attach_alternative(html_body, "text/html")
+        msg.send()
